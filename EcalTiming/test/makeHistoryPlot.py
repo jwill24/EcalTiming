9ddc8570ed10a98a8c8deef25f728c765e03a37b
Badder Marzocchi <badder.marzocchi@cern.ch>
2017-10-03 12:42:19 +0200

    fix correction and absolute timing

--- EcalTiming/test/makeHistoryPlot.py
+++ EcalTiming/test/makeHistoryPlot.py
@@ -7,11 +7,11 @@ from historyPlot_utils import calibFromXML
 from historyPlot_utils import calibFromDAT
 
 def usage():
-    print "Usage: python makeHistoryPlot.py --tag=[tag] --year=[year]"
-    print "Usage: python makeHistoryPlot.py --inList=[inList]"
+    print "Usage: python makeHistoryPlot.py --tag=[tag] --year=[year] (--run=[run] --absTime)"
+    print "Usage: python makeHistoryPlot.py --inList=[inList] (--run=[run] --absTime)"
     
 try:
-     opts, args = getopt.getopt(sys.argv[1:], "t:y:i:h", ["tag=","year=","inList=","help"])
+     opts, args = getopt.getopt(sys.argv[1:], "t:y:i:r:ah", ["tag=","year=","inList=","run=","absTime","help"])
 
 except getopt.GetoptError:
      #* print help information and exit:*
@@ -21,7 +21,9 @@ except getopt.GetoptError:
 
 tag = ""
 year = ""
+run = ""
 inList = ""
+absTime = False
 help = False
 for opt, arg in opts:
     
@@ -29,8 +31,12 @@ for opt, arg in opts:
         tag = arg
      if opt in ("--year"):
         year = arg
+     if opt in ("--run"):
+        run = arg
      if opt in ("--inList"):
-        inList = arg    
+        inList = arg  
+     if opt in ("--absTime"):
+        absTime = True    
      if opt in ("--help"):
         help = True     
 
@@ -38,20 +44,20 @@ if(help == True):
    usage()
    sys.exit(2)
 
-if((tag == "" and year == "" and inList == "") or (tag != "" and year == "") or (tag == "" and year != "")):
+if((tag == "" and year == "" and run == "" and inList == "") or (tag != "" and year == "") or (tag == "" and year != "")):
    usage()
    sys.exit(2)
 
-if(inList == ""):     
+if(tag != ""):
    print "tag          = ",tag
+if(year != ""):
    print "year         = ",year
-else:
-   print "inList       = ",inList 
-
-def printme( str ):
-   "This prints a passed string into this function"
-   print str
-   return
+if(run != ""):
+   print "run          = ",run
+if(inList != ""):
+   print "inList       = ",inList  
+if(absTime == True):
+   print "absTime      = ",absTime  
 
 #gStyle.SetOptStat(0)
 
@@ -110,7 +116,7 @@ timeStamp_end = timeStamp_list[len(timeStamp_list)-1]
 
 allCalib_list.sort()
 y_min = allCalib_list[0]-0.05
-y_max = allCalib_list[len(allCalib_list)-1]+0.2
+y_max = allCalib_list[len(allCalib_list)-1]+0.3
 
 g_EBMinus.SetMarkerColor(600)
 g_EBMinus.SetLineColor(600)
@@ -180,7 +186,10 @@ g_EEPlus.GetYaxis().SetTitleSize(0.05)
 g_EEPlus.GetYaxis().SetTitleOffset(1.1)
 g_EEPlus.GetYaxis().SetTitleFont(42)
 
-test = TH1F("test","Ecal Timing ["+str(year)+"D]",1000,float(timeStamp_begin-0.001e+9),float(timeStamp_end+0.001e+9))
+if(absTime == False):
+   test = TH1F("test","Ecal Timing ["+str(year)+str(run)+"]",1000,float(timeStamp_begin-172800),float(timeStamp_end+172800))
+else:
+   test = TH1F("test","Ecal Absolute Timing ["+str(year)+str(run)+"]",1000,float(timeStamp_begin-172800),float(timeStamp_end+172800))
 test.SetStats(0)
 test.SetFillColor(1)
 test.SetFillStyle(3004)
@@ -196,7 +205,10 @@ test.GetXaxis().SetLabelOffset(0.007)
 test.GetXaxis().SetLabelSize(0.04)
 test.GetXaxis().SetTitleSize(0.05)
 test.GetXaxis().SetTitleFont(42)
-test.GetYaxis().SetTitle("Average Time[ns]")
+if(absTime == False):
+   test.GetYaxis().SetTitle("Average Time [ns]")
+else:
+   test.GetYaxis().SetTitle("Absolute Time [ns]")
 test.GetYaxis().SetLabelFont(42)
 test.GetYaxis().SetLabelOffset(0.007)
 test.GetYaxis().SetLabelSize(0.05)
@@ -221,6 +233,11 @@ line_IOV1_calib = TLine(1494460800,float(y_min),1494460800,float(y_max))
 line_IOV1_calib.SetLineColor(633)
 line_IOV1_calib.SetLineStyle(8)
 line_IOV1_calib.SetLineWidth(2)
+
+line_IOV1_inBad = TLine(1494806400,float(y_min),1494806400,float(y_max))
+line_IOV1_inBad.SetLineColor(880)
+line_IOV1_inBad.SetLineStyle(8)
+line_IOV1_inBad.SetLineWidth(2)
 
 line_IOV1_in = TLine(1496880000,float(y_min),1496880000,float(y_max))
 line_IOV1_in.SetLineColor(417)
@@ -262,6 +279,8 @@ c1.SetGrid()
 test.Draw("H")
 if(float(timeStamp_begin-0.001e+9)<=1494460800. and float(timeStamp_end+0.001e+9)>=1494460800.):
    line_IOV1_calib.Draw("same")
+if(float(timeStamp_begin-0.001e+9)<=1494806400. and float(timeStamp_end+0.001e+9)>=1494806400.):
+   line_IOV1_inBad.Draw("same")
 if(float(timeStamp_begin-0.001e+9)<=1496880000. and float(timeStamp_end+0.001e+9)>=1496880000.):
    line_IOV1_in.Draw("same")
 if(float(timeStamp_begin-0.001e+9)<=1497744000. and float(timeStamp_end+0.001e+9)>=1497744000.):
@@ -281,8 +300,8 @@ g_EBPlus.Draw("P,same")
 g_EEMinus.Draw("P,same")
 g_EEPlus.Draw("P,same")
 leg.Draw("same")
-c1.SaveAs("Timing_History_"+str(year)+"D.png","png")
-c1.SaveAs("Timing_History_"+str(year)+"D.pdf","pdf") 
+c1.SaveAs("Timing_History_"+str(year)+str(run)+".png","png")
+c1.SaveAs("Timing_History_"+str(year)+str(run)+".pdf","pdf") 
 
 if(inList == ""): 
    command = os.system("rm IOVs_tmp")
--- EcalTiming/test/makeTimeCalibConstantsTag_step1.py
+++ EcalTiming/test/makeTimeCalibConstantsTag_step1.py
@@ -1,45 +1,85 @@
 #!/usr/bin/env python
 
-import sys, math, os, collections, getopt, subprocess, time, shutil
+import sys, math, os, collections, getopt, subprocess, time, shutil, datetime
 from array import array
 
+from historyPlot_utils import makeAbsTimingXML
+
 def usage():
-    print "Usage: python makeTimeCalibConstantsTag_step1.py --tag=[tag] --calib=[calib] --output=[output]"
+    print "Usage: python makeTimeCalibConstantsTag_step1.py --tag=[tag] --inList=[inList]"
+    print "Usage: python makeTimeCalibConstantsTag_step1.py --tag=[tag] --calib=[calib]"
+    print "Usage: python makeTimeCalibConstantsTag_step1.py --payload=[payload] --inList=[inList]"
+    print "Usage: python makeTimeCalibConstantsTag_step1.py --payload=[payload] --calib=[calib]"
     
 try:
-     opts, args = getopt.getopt(sys.argv[1:], "t:c:o:", ["tag=","calib=","output="])
+     opts, args = getopt.getopt(sys.argv[1:], "t:i:c:p:h", ["tag=","inList=","calib=","payload=","help"])
 
 except getopt.GetoptError:
      #* print help information and exit:*
      usage()
      sys.exit(2)
 
+tag = ""
+calib = ""
+inList = ""
+payload = ""
+help = False
 for opt, arg in opts:
     
      if opt in ("--tag"):
         tag = arg
      if opt in ("--calib"):
         calib = arg
-     if opt in ("--output"):
-        output= arg     
-     
-print "input tag          = ",tag
-print "input calibration  = ",calib
-print "output IOV         = ",output 
+     if opt in ("--inList"):
+        inList = arg 
+     if opt in ("--payload"):
+        payload = arg    
+     if opt in ("--help"):
+        help = True     
 
-#dump last IOV xml
-print "---- Dumping last IOV ----"
+if(help == True):
+   usage()
+   sys.exit(2)
 
-#command = os.system("conddb list EcalTimeCalibConstants_validation > IOVs_tmp")
-command = os.system("conddb list "+str(tag)+" > IOVs_tmp")
-with open('IOVs_tmp') as f_IOVs:
-     data_IOVs = f_IOVs.read()
+goodMode = False
 
-lines_IOVs = data_IOVs.splitlines()
-line_IOVs_split = lines_IOVs[len(lines_IOVs)-2].split()
-print "Payload: ",line_IOVs_split[5]
-command = os.system("conddb dump "+ str(line_IOVs_split[5]) +" > dump_tmp")
+if((tag != "" and inList != "" and payload == "" and calib == "") or (tag != "" and inList == "" and payload == "" and calib != "") or (tag == "" and inList != "" and payload != "" and calib == "") or (tag == "" and inList == "" and payload != "" and calib != "")):
+   goodMode = True
+if(goodMode == False):
+   usage()
+   sys.exit(2)
 
+if(tag != ""):
+   print "tag          = ",tag
+if(payload != ""):
+   print "payload      = ",payload
+if(inList != ""):
+   print "inList       = ",inList 
+if(calib != ""):
+   print "calib        = ",run 
+
+IOVs_date = []
+IOVs_time = []
+IOVs_info = []
+
+#dump IOVs xml
+print "---- Dumping IOVs info ----"
+
+if(tag != ""):
+   command = os.system("conddb list "+str(tag)+" > IOVs_tmp")
+   with open("IOVs_tmp") as f_IOVs:
+      data_IOVs = f_IOVs.read()
+      lines_IOVs = data_IOVs.splitlines()
+      for pos,x in enumerate(lines_IOVs):
+         if(pos > 2 and pos < len(lines_IOVs)-1):
+            line_IOVs_split = lines_IOVs[pos].split()
+            IOV_time = line_IOVs_split[2].split("-")
+            date = IOV_time[2]+"/"+IOV_time[1]+"/"+IOV_time[0]
+            timeStamp = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+            IOVs_date.append(date)
+            IOVs_time.append(timeStamp)
+            IOVs_info.append(x)
+            
 #make crystal maps
 print "---- Making crystal maps ----"
 
@@ -73,62 +113,69 @@ for pos,x in enumerate(lines_cryEE):
 #Reading the calibration file
 print "---- Reading the calibration file ----"
 
-#with open("/afs/cern.ch/work/b/bmarzocc/ECAL_Timing/CMSSW_9_2_6/src/EcalTiming/EcalTiming/output/ecalTiming-corr_run299481.dat") as f_interCalib:
-with open(str(calib)) as f_interCalib:
-        data_interCalib = f_interCalib.read()
-lines_interCalib = data_interCalib.splitlines() 
-for pos,x in enumerate(lines_interCalib):
-    lines_interCalib_split = x.split()
-    if(pos>=0 and pos<=60496): 
-       if(lines_interCalib_split[3] == 'nan' or abs(float(lines_interCalib_split[3]))<=(abs(float(lines_interCalib_split[4]))/math.sqrt(float(lines_interCalib_split[5])))):
-          timeIntercalib_EB[lines_interCalib_split[7]]=float(0.) 
-          print "WARNING:",lines_interCalib_split[0],lines_interCalib_split[1],lines_interCalib_split[2],"- anomalous crystal, setting calibration to 0"
-       else:
-          timeIntercalib_EB[lines_interCalib_split[7]]=float(lines_interCalib_split[3])
-       crystals_EB[lines_interCalib_split[7]] = bool(True)
-    else:  
-       if(lines_interCalib_split[3] == 'nan' or abs(float(lines_interCalib_split[3]))<=(abs(float(lines_interCalib_split[4]))/math.sqrt(float(lines_interCalib_split[5])))):
-          timeIntercalib_EE[lines_interCalib_split[7]]=float(0.) 
-          print "WARNING:",lines_interCalib_split[0],lines_interCalib_split[1],lines_interCalib_split[2],"- anomalous crystal, setting calibration to 0"
-       else:
-          timeIntercalib_EE[lines_interCalib_split[7]]=float(lines_interCalib_split[3])
-       crystals_EE[lines_interCalib_split[7]] = bool(True)
+if(inList != ""):
+   with open(str(inList)) as f_interCalib:
+      data_interCalib = f_interCalib.read()
+   lines_interCalib = data_interCalib.splitlines()
+   inList_absCalib = inList.replace(".dat", "_absTiming.dat") 
 
-#making the new IOV xml
-print "---- Making new IOV xml ----"
- 
-with open("dump_tmp") as f_dump:
-        data_dump = f_dump.read()
-lines_dump = data_dump.splitlines()  
+if(payload != ""):
+   print "---> Dumping the pyload: ",payload 
+   command = os.system("conddb dump "+ str(payload) +" > dump_tmp")
 
-#f_tag = open("EcalTimeCalibConstants_new_IOV_299481.xml","w")
-f_tag = open(str(output),"w")
-for pos,x in enumerate(lines_dump):
-    x_intro = str(x)+"\n"
-    if(pos<8 or (pos>61207 and pos<61214) or pos>75861):
-       x = str(x)+"\n"
-       f_tag.write(x)
-    elif(pos>=8 and pos<=61207): 
-       x = x.replace("<item>", "")
-       x = x.replace("</item>", "")
-       if(crystals_EB[pos_EB[pos-8]] == True):
-          x = float(x)-float(timeIntercalib_EB[pos_EB[pos-8]]) 
-       else:
-          x = float(x) 
-       x = "{:.9e}".format(x)
-       x_abs = "			<item>"+str(x)+"</item> \n"
-       f_tag.write(x_abs)
-    elif(pos>=61214 and pos<=75861):
-       x = x.replace("<item>", "")
-       x = x.replace("</item>", "")
-       if(crystals_EE[pos_EE[pos-61214]] == True):
-          x = float(x)-float(timeIntercalib_EE[pos_EE[pos-61214]]) 
-       else:
-          x = float(x) 
-       x = "{:.9e}".format(x)
-       x_abs = "			<item>"+str(x)+"</item> \n"
-       f_tag.write(x_abs)
-f_tag.close()
+if(calib == ""):
+   f_absCalib = open(str(inList_absCalib),"w")
+   for pos,x in enumerate(lines_interCalib):
+      if(lines_interCalib[pos].find("#") != -1):
+         continue
+      output_absCalib = x.replace(".dat", ".xml")
+      output_absCalib = output_absCalib.replace("-corr_", "-abs_")
+      lines_interCalib_split = lines_interCalib[pos].split("/")
+      interCalib_time = lines_interCalib_split[len(lines_interCalib_split)-1].split("_")
+      if(interCalib_time[3].find(".dat") != -1):
+         date = interCalib_time[3].replace(".dat", "")+"/"+interCalib_time[2]+"/"+interCalib_time[1]
+         timeStamp = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+         if(payload == ""):
+            icount = 0
+            for pos2,x2 in enumerate(IOVs_time):
+               if(float(IOVs_time[pos2])>=float(timeStamp)):
+                  icount = pos2-1
+                  break
+               if(float(IOVs_time[pos2])<float(timeStamp) and pos2 == len(IOVs_time)-1):
+                  icount = len(IOVs_time)-1
+                  break
+            print date," ---> ",IOVs_info[icount]
+            line_IOVs_split = IOVs_info[icount].split()
+            command = os.system("conddb dump "+ str(line_IOVs_split[5]) +" > dump_tmp")
+         makeAbsTimingXML(lines_interCalib[pos], timeIntercalib_EB, timeIntercalib_EE, pos_EB, pos_EE, crystals_EB, crystals_EE, output_absCalib)
+         f_absCalib.write(str(output_absCalib)+"\n")
+      else:
+         print "WARNING: wrong import format! Skipping this IOV"  
+   f_absCalib.close()
+else:
+   output_absCalib = calib.replace(".dat", ".xml")
+   output_absCalib = output_absCalib.replace("-corr_", "-abs_")
+   lines_interCalib_split = calib.split("/")
+   interCalib_time = lines_interCalib_split[len(lines_interCalib_split)-1].split("_")
+   if(interCalib_time[3].find(".dat") != -1):
+      date = interCalib_time[3].replace(".dat", "")+"/"+interCalib_time[2]+"/"+interCalib_time[1]
+      timeStamp = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+      if(payload == ""):
+         icount = 0
+         for pos2,x2 in enumerate(IOVs_time):
+            if(float(IOVs_time[pos2])>=float(timeStamp)):
+               icount = pos2-1
+               break
+            if(float(IOVs_time[pos2])<float(timeStamp) and pos2 == len(IOVs_time)-1):
+               icount = len(IOVs_time)-1
+               break
+         print date," ---> ",IOVs_info[icount]
+         line_IOVs_split = IOVs_info[icount].split()
+         command = os.system("conddb dump "+ str(line_IOVs_split[5]) +" > dump_tmp")
+      makeAbsTimingXML(calib, timeIntercalib_EB, timeIntercalib_EE, pos_EB, pos_EE, crystals_EB, crystals_EE, output_absCalib)
+      print "---> Absolute Timing file produced: ",output_absCalib
+   else:
+      print "WARNING: wrong import format! Skipping this IOV"  
 
 command = os.system("rm IOVs_tmp")
 command = os.system("rm dump_tmp")
